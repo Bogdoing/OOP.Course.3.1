0d0578f88108b0c3f323fa62bd51b1b3d1ab3569
Bogdoing <bogdoingg@gmail.com>
2022-11-20 22:36:51 +0300

    0.1

---
+++ OOP.Course31.1/Forms/Form2add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP.Course31._1.Forms
+{
+    public partial class Form2add : Form
+    {
+        public Form2add()
+        {
+            InitializeComponent();
+        }
+    }
+}
